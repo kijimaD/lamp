97f5606ec492ee0bb3281c8a1ea404053b4b57bc
kijimaD <norimaking777@gmail.com>
2023-03-08 00:33:06 +0900

    feat: astdump subcommand

--- main.go
+++ main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"go/ast"
+	"go/parser"
+	"go/token"
 	"os"
 
 	"github.com/kijimaD/lamp/myscope"
@@ -13,7 +17,7 @@ func main() {
 	myscopeCmd := flag.NewFlagSet("myscope", flag.ExitOnError)
 
 	if len(os.Args) < cmdCount {
-		panic("expected 'myscope' subcommands")
+		panic("expected 'myscope' | 'astdump' subcommands")
 	}
 
 	switch os.Args[1] {
@@ -27,6 +31,20 @@ func main() {
 		}
 		c := myscope.NewClient(file, os.Stdout)
 		c.Run()
+	case "astdump":
+		if err := myscopeCmd.Parse(os.Args[2:]); err != nil {
+			panic(err)
+		}
+		fset := token.NewFileSet()
+		astf, err := parser.ParseFile(fset, os.Args[2], nil, parser.Mode(0))
+		if err != nil {
+			panic(err)
+		}
+
+		for _, d := range astf.Decls {
+			ast.Print(fset, d)
+			fmt.Println() //nolint:forbidigo
+		}
 	default:
 		panic("not found subcommand")
 	}
